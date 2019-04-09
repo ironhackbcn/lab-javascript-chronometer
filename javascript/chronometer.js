4ea551cad729fe2ad18e89f1fb9810a2a04a115e
Katharine Cooney <katharinecooney@gmail.com>
2019-04-09 21:24:42 +0200

    Add iterations 1 - 4

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,57 +1,64 @@
-// Constructor
 function Chronometer() {
-
-    this.currentTime = 0;
-    this.intervalId = null;
-  
+  this.currentTime = 0;
+  this.intervalId = null;
 }
 
- Chronometer.prototype.startClick = function () {
-this.intervalId = setInterval(() =>{
-  this.currentTime+= 1;
-}, 1000)
+Chronometer.prototype.startClick = function() {
+  this.intervalId = setInterval(() => {
+    this.currentTime++;
+
+    // save our time in a variable so we can pass it to other functions
+    const time = this.setTime();
+    this.printTimeCallback(time);
+    // printTimeCallback(time);
+  }, 1000); 
+}
+
+Chronometer.prototype.setMinutes = function () {
+  return Number(Math.floor(this.currentTime / 60));  
 };
 
- Chronometer.prototype.setMinutes = function () {
-  var minutes = Math.floor(this.currentTime/ 60)
-  return minutes;
+Chronometer.prototype.setSeconds = function () {
+  return this.currentTime % 60;
 };
 
- Chronometer.prototype.setSeconds = function () {
-  var seconds = this.currentTime % 60;
-  return seconds;
- };
+Chronometer.prototype.twoDigitsNumber = function (value) {
+  value = String(value);
+  if(value < 10) {
+    return 0 + value; 
+  } else {
+    return value;
+  }
+};
 
- Chronometer.prototype.twoDigitsNumber = function () {
- if (minutes < 10 && seconds < 10) {
-   minutes = "0" + minutes;
-   seconds = "0" + seconds;
- } else if (minutes < 10) {
-  minutes = "0" + minutes;
- } else if (seconds < 10){
-  seconds = "0" + seconds;
- } else {
-   return minutes;
- }
+Chronometer.prototype.setTime = function () {
+  var minutes = this.twoDigitsNumber(this.setMinutes());
+  var seconds = this.twoDigitsNumber(this.setSeconds());
 
- };
-
-// Chronometer.prototype.setTime = function () {
-
-// };
+  
+  // we need to return the minutes and seconds so we can use this data in other functions
+  // we can only return one thing, so we put both the minutes and seconds in one array
+  return [minutes, seconds];
+};
 
 // Chronometer.prototype.setMilliseconds = function () {
 
 // };
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+Chronometer.prototype.stopClick = function () {
+  clearInterval(this.intervalId);  
+};
 
-// Chronometer.prototype.resetClick = function () {
-
-// };
+Chronometer.prototype.resetClick = function () {
+  this.currentTime = 0;
+  // this.startClick();
+};
 
 // Chronometer.prototype.splitClick = function () {
 
 // };
+
+Chronometer.prototype.setPrintTime = function(callback) 
+{
+  this.printTimeCallback = callback;
+}
--- javascript/main.js
+++ javascript/main.js
@@ -1,4 +1,5 @@
 var chronometer = new Chronometer();
+chronometer.setPrintTime(printTime);
 var btnLeft     = document.getElementById('btnLeft');
 var btnRight    = document.getElementById('btnRight');
 var minDec      = document.getElementById('minDec');
@@ -8,52 +9,100 @@ var secUni      = document.getElementById('secUni');
 var milDec      = document.getElementById('milDec');
 var milUni      = document.getElementById('milUni');
 
-function printTime() {
-
+function printTime(time) {
+  // console.log(time) will return [00, 01], for example
+  printMinutes(time[0]);
+  printSeconds(time[1]);
 }
 
-function printMinutes() {
-
+function printMinutes(minutes) {
+  minDec.innerHTML = minutes[0];
+  minUni.innerHTML = minutes[1];
 }
 
-function printSeconds() {
-
+function printSeconds(seconds) {
+  secDec.innerHTML = seconds[0];
+  secUni.innerHTML = seconds[1];
 }
 
 function printMilliseconds() {
 
 }
 
-function printSplit() {
+// QUESTION - I want to remove the variables splitLi and splitOl and place them at the top of the scrip along with the other selected elements, but when i do this, I am only allowed to add one splitLi to my splitOl list, and it just keeps updating the splitLi! WHY?
 
+function printSplit() {
+  var splitLi = document.createElement('li');
+  var splitOl = document.getElementById('splits');
+  var time = chronometer.setTime();
+  var minutes = time[0];
+  var seconds = time[1];
+  splitLi.innerHTML = `${minutes}:${seconds}`;
+
+  splitOl.appendChild(splitLi);
 }
 
 function clearSplits() {
-
+  var splitOl = document.getElementById('splits');
+  var liNodeList = document.querySelectorAll('li');
+  var liArray = [...liNodeList];
+  for(var i = 0; i < liArray.length; i++) {
+    splitOl.removeChild(liArray[i]);
+  }
 }
 
 function setStopBtn() {
-
+  btnLeft.className = "btn stop";
+  btnLeft.innerHTML = "STOP";
 }
 
 function setSplitBtn() {
-
+  btnRight.className = "btn split";
+  btnRight.innerHTML = "SPLIT";
 }
 
 function setStartBtn() {
-
+  btnLeft.className = "btn start";
+  btnLeft.innerHTML = "START";
 }
 
 function setResetBtn() {
-
+  btnRight.className = "btn reset";
+  btnRight.innerHTML = "RESET";
 }
 
-// Start/Stop Button
-btnLeft.addEventListener('click', function () {
+//////////// START AND STOP BUTTONS ////////////
 
+btnLeft.addEventListener('click', function () {
+  if (btnLeft.className === 'btn start') {
+    setStopBtn();
+    setSplitBtn();
+    chronometer.startClick();
+  } 
+
+  else if (btnLeft.className === 'btn stop') {
+    setStartBtn();
+    setResetBtn();
+    chronometer.stopClick();
+  }
 });
 
-// Reset/Split Button
+//////////// RESET AND SPLIT BUTTONS ////////////
+
 btnRight.addEventListener('click', function () {
 
+  if(btnRight.className === 'btn split') {
+    printSplit();
+  }
+
+  if(btnRight.className === 'btn reset') {
+    minDec.innerHTML = 0;
+    minUni.innerHTML = 0;
+    secDec.innerHTML = 0;
+    secUni.innerHTML = 0;
+    chronometer.resetClick();
+    clearSplits();
+  }
 });
+
+
