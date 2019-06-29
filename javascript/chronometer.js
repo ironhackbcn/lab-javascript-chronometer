dc75c2bcd2844c33835083c16f5d559dc6be2039
Johan Bautista <jsbparra@gmail.com>
2019-06-29 09:00:25 +0200

    it 1

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,40 +1,16 @@
-// Constructor
-// function Chronometer() {
+function Chronometer() {
+  this.currentTime = 0;
+  this.intervalId = 0;
+}
 
-// }
+Chronometer.prototype.startClick = function() {};
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.setMinutes = function() {};
 
-// };
-
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
-
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
-
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
-
-// Chronometer.prototype.setTime = function () {
-
-// };
-
-// Chronometer.prototype.setMilliseconds = function () {
-
-// };
-
-// Chronometer.prototype.stopClick = function () {
-  
-// };
-
-// Chronometer.prototype.resetClick = function () {
-
-// };
-
-// Chronometer.prototype.splitClick = function () {
-
-// };
+Chronometer.prototype.setSeconds = function() {};
+Chronometer.prototype.twoDigitsNumber = function() {};
+Chronometer.prototype.setTime = function() {};
+Chronometer.prototype.setMilliseconds = function() {};
+Chronometer.prototype.stopClick = function() {};
+Chronometer.prototype.resetClick = function() {};
+Chronometer.prototype.splitClick = function() {};
