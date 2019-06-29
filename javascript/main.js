86cda593f6f8a040683e27cac9a106765bb1c251
Antonio Rivera <antonioriver@gmail.com>
2019-06-29 21:19:22 +0200

    solo falta split - comenzado pero no terminado

--- javascript/main.js
+++ javascript/main.js
@@ -1,13 +1,12 @@
 var chronometer = new Chronometer();
-var btnLeft = document.getElementById('btnLeft');
-var btnRight = document.getElementById('btnRight');
-var minDec = document.getElementById('minDec');
-var minUni = document.getElementById('minUni');
-var secDec = document.getElementById('secDec');
-var secUni = document.getElementById('secUni');
-var milDec = document.getElementById('milDec');
-var milUni = document.getElementById('milUni');
-
+var btnLeft = document.getElementById("btnLeft");
+var btnRight = document.getElementById("btnRight");
+var minDec = document.getElementById("minDec");
+var minUni = document.getElementById("minUni");
+var secDec = document.getElementById("secDec");
+var secUni = document.getElementById("secUni");
+var milDec = document.getElementById("milDec");
+var milUni = document.getElementById("milUni");
 
 function printTime() {
   minDec.innerHTML = printMinutes()[0];
@@ -18,77 +17,83 @@ function printTime() {
 
 function printMinutes() {
   var time = chronometer.setTime()[0];
-  console.log("time a ver minutos " + time)
-  var minutes = time.split('');
+  console.log("time a ver minutos " + time);
+  var minutes = time.split("");
 
   return minutes;
-
 }
 
 function printSeconds() {
   var time = chronometer.setTime()[1];
-  var seconds = time.split('');
+  var seconds = time.split("");
 
   return seconds;
 }
 
-function printMilliseconds() {
+function printMilliseconds() {}
 
+function printSplit(minDecSplit, minUniSplit, secDecSplit, secUniSplit) {
+  minDec.innerHTML = minDecSplit;
+  minUni.innerHTML = minDecSplit;
+  secDec.innerHTML = minDecSplit;
+  secUni.innerHTML = minDecSplit;
 }
 
-function printSplit() {
-
-}
-
-function clearSplits() {
-
-}
+function clearSplits() {}
 
 function setStopBtn() {
   chronometer.stopClick();
-  btnLeft.innerHTML = 'START'
-  btnRight.innerHTML = 'RESET'
-
+  btnLeft.innerHTML = "START";
+  btnRight.innerHTML = "RESET";
 }
 
-function setSplitBtn() {}
+function setSplitBtn() {
+  var minDecSplit = minDec.textContent;
+  var minUniSplit = minUni.textContent;
+  var secDecSplit = secDec.textContent;
+  var secUniSplit = secUni.textContext;
+
+  console.log("TCL: setSplitBtn -> secUniSplit", secUniSplit);
+  console.log("TCL: setSplitBtn -> secDecSplit", secDecSplit);
+  console.log("TCL: setSplitBtn -> minUniSplit", minUniSplit);
+  console.log("TCL: setSplitBtn -> minDecSplit", minDecSplit);
+  printSplit(minDecSplit, minUniSplit, secDecSplit, secUniSplit);
+}
 
 function setStartBtn() {
-  chronometer.startClick()
-  printTime()
-  btnLeft.innerHTML = 'STOP'
-  btnRight.innerHTML = 'SPLIT'
-}
+  chronometer.startClick();
 
+  btnLeft.innerHTML = "STOP";
+  btnRight.innerHTML = "SPLIT";
+}
 
 function setResetBtn() {
   chronometer.resetClick();
-  printTime()
-
+  printTime();
 }
 
-var stateStartStopButton = 1
+var stateStartStopButton = 1;
 
 // Start/Stop Button
-btnLeft.addEventListener('click', function () {
-  stateStartStopButton++
+btnLeft.addEventListener("click", function() {
+  stateStartStopButton++;
   if (stateStartStopButton % 2 == 0) {
     setStartBtn();
   } else {
-    console.log(stateStartStopButton)
+    console.log(stateStartStopButton);
     setStopBtn();
   }
-
 });
 
-var stateRestSplitButton = 1
+var stateRestSplitButton = 1;
 
 // Reset/Split Button
-btnRight.addEventListener('click', function () {
+btnRight.addEventListener("click", function() {
   stateRestSplitButton++;
   if (stateStartStopButton % 2 != 0) {
     setResetBtn();
+  } else if (stateRestSplitButton % 2 != 0) {
+    setSplitBtn();
   } else {
-
   }
 });
