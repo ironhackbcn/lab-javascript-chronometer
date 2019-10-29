fa3a42a4332aede2ea281894bc3f4fd9a218d2a6
Iván Francés Alcántara <ifrancesalcantara@gmail.com>
2019-10-29 20:34:34 +0100

    done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,40 +1,46 @@
 // Constructor
 // function Chronometer() {
+function Chronometer () {
+    this.currentTime = 0;
+    this.intervalId = 1000;
+    
+    Chronometer.prototype.startClick = function() {
+        setInterval(()=>{
+            this.currentTime += 1;
+            this.setTime();
+        }, this.intervalId);
+    }
+    Chronometer.prototype.setMinutes = function () {
+        return Math.round(parseInt((this.currentTime/60)))
+    }
+    Chronometer.prototype.setSeconds = function () {
+        if (this.currentTime==0){
+            return 00
+        } else if (this.currentTime>0 && this.currentTime<10){
+            return parseInt(`0${this.currentTime}`)
+        
+        } else {
+            return this.currentTime%60
+        }
+    }
+    Chronometer.prototype.twoDigitsNumber = function (number) {
+        console.log(number);
+        if (number < 10){
+            return `0${number}`;
+        } else {
+            return `${number}`;
+        }
+    }
+    Chronometer.prototype.setTime = function () {
+        let minutes = this.twoDigitsNumber(this.setMinutes());
+        let seconds = this.twoDigitsNumber(this.setSeconds());
+    }
 
-// }
+    Chronometer.prototype.stopClick = function () {
+        clearInterval(this.startClick)
+    }
 
-// Chronometer.prototype.startClick = function () {
-
-// };
-
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
-
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
-
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
-
-// Chronometer.prototype.setTime = function () {
-
-// };
-
-// Chronometer.prototype.setMilliseconds = function () {
-
-// };
-
-// Chronometer.prototype.stopClick = function () {
-  
-// };
-
-// Chronometer.prototype.resetClick = function () {
-
-// };
-
-// Chronometer.prototype.splitClick = function () {
-
-// };
+    Chronometer.prototype.resetClick = function () {
+        this.currentTime = 0
+    }
+}
