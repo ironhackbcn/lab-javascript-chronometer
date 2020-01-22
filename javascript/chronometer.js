dbf2cd06b0d5ab1051cad71cd3e93442893ce0e1
Val GZ <valmgisbert@gmail.com>
2020-01-22 20:07:09 +0100

    done

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,40 +1,57 @@
 // Constructor
-// function Chronometer() {
-
-// }
-
-// Chronometer.prototype.startClick = function () {
-
-// };
-
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
-
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
-
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
-
-// Chronometer.prototype.setTime = function () {
-
-// };
+function Chronometer() {
+    this.currentTime = 0;
+    this.intervalId = 0;
+    this.callBack = function () {
+        printTime();
+    }
+}
+Chronometer.prototype.startClick = function () {
+    intervalId = setInterval (() => {
+        this.currentTime++;
+        this.setTime();
+        this.callBack();
+    }, 1000);
+};
+Chronometer.prototype.setMinutes = function () {
+    var minutes = Math.floor(this.currentTime / 60);
+    return minutes;
+};
+Chronometer.prototype.setSeconds = function () {
+    var seconds = this.currentTime % 60;
+    return seconds;
+};
+Chronometer.prototype.twoDigitsNumber = function (num) {
+        var str = "0" + num;
+        return str.slice(-2);
+}
+Chronometer.prototype.setTime = function () {
+    var minutes = this.twoDigitsNumber(this.setMinutes()); //this gives me a string and we can use index
+    var seconds = this.twoDigitsNumber(this.setSeconds());
+    return [minutes, seconds];
+};
 
 // Chronometer.prototype.setMilliseconds = function () {
-
 // };
 
-// Chronometer.prototype.stopClick = function () {
-  
-// };
+Chronometer.prototype.stopClick = function () {
+    clearInterval(intervalId);
 
-// Chronometer.prototype.resetClick = function () {
+};
+Chronometer.prototype.resetClick = function () { //callback is only for funcs
+    this.currentTime = clearInterval(this.currentTime);
+    this.currentTime = 0;
 
-// };
+    var minDec = document.getElementById('minDec');
+    var minUni = document.getElementById('minUni');
+    var secDec = document.getElementById('secDec');
+    var secUni = document.getElementById('secUni');
+
+    minDec.innerHTML = 0;
+    minUni.innerHTML = 0;
+    secDec.innerHTML = 0;
+    secUni.innerHTML = 0;
+};
 
 // Chronometer.prototype.splitClick = function () {
-
 // };
--- javascript/main.js
+++ javascript/main.js
@@ -7,34 +7,43 @@ var secDec      = document.getElementById('secDec');
 var secUni      = document.getElementById('secUni');
 var milDec      = document.getElementById('milDec');
 var milUni      = document.getElementById('milUni');
+var orderedList = document.querySelector('#splits');
 
 
 function printTime() {
-
+    printSeconds();
+    printMinutes();
 }
 
 function printMinutes() {
-
+    var minutes = chronometer.setTime()[0];
+    //do we call the vars with the funcs inside from setTime? - yes
+    minDec.innerHTML = minutes[0];
+    minUni.innerHTML = minutes[1];
 }
 
 function printSeconds() {
-
+    var seconds = chronometer.setTime()[1];
+    secDec.innerHTML = seconds[0]; 
+    secUni.innerHTML = seconds[1];
+    // same as up
+  
 }
 
 function printMilliseconds() {
-
+    //uhhhhh no
 }
 
 function printSplit() {
-
+    //is the ordered list added from the function itself or do we add it from the skeleton a set number? -yes
 }
 
 function clearSplits() {
-
+    //for this do we use stopTime? - yes
 }
 
 function setStopBtn() {
-
+    chronometer.stopClick();
 }
 
 function setSplitBtn() {
@@ -42,19 +51,61 @@ function setSplitBtn() {
 }
 
 function setStartBtn() {
-
+        printTime();
 }
 
 function setResetBtn() {
-
+    chronometer.resetClick();
+    orderedList.remove
 }
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
+    //how do we make it recognize that the chron is working or stopped?
 
-});
+    if (btnLeft.className === "btn start") {
+        setStartBtn();
+        btnLeft.className = "btn stop";
+        btnLeft.innerHTML = "STOP";
+        
+
+        btnRight.innerHTML = "SPLIT"
+        btnRight.className = "btn split";
+
+        chronometer.startClick();
+    } else if (btnLeft.className === "btn stop"){
+        setStopBtn();
+        btnLeft.innerHTML = "START";
+        btnLeft.className = "btn start";
+
+        btnRight.innerHTML = "RESET";
+        btnRight.className = "btn reset";
+    }
+    }
+);
+
+
 
 // Reset/Split Button
 btnRight.addEventListener('click', function () {
+    if (btnRight.className === 'btn reset') {
+        setResetBtn();
+        var splitsEl = document.getElementById('splits');
+        while (splitsEl) {
+            splitsEl.removeChild(splitsEl.firstChild);
+        };
+    
 
+
+    } else if (btnRight.className === 'btn split') {
+        var newItem = document.createElement('li');
+        var updatedTime = [...chronometer.setTime()];
+        newItem.className = "splitElement";
+        newItem.innerHTML = `${updatedTime[0]}:${updatedTime[1]}`;
+        orderedList.appendChild(newItem);
+
+        
+    }
+
+    //if the instructions say the leftbutton and we put the btnright iterations there, we don't separate them right? bc the event is from the left button 
 });
