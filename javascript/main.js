3ef67a46362c5d048c0a4320af05aebeda5f988c
JuiceDrinker <subramanianaditya42@gmail.com>
2020-01-22 15:32:41 +0100

    basic dom + logic done

--- javascript/main.js
+++ javascript/main.js
@@ -1,24 +1,32 @@
 var chronometer = new Chronometer();
-var btnLeft     = document.getElementById('btnLeft');
-var btnRight    = document.getElementById('btnRight');
-var minDec      = document.getElementById('minDec');
-var minUni      = document.getElementById('minUni');
-var secDec      = document.getElementById('secDec');
-var secUni      = document.getElementById('secUni');
-var milDec      = document.getElementById('milDec');
-var milUni      = document.getElementById('milUni');
+var btnLeft = document.getElementById('btnLeft');
+var btnRight = document.getElementById('btnRight');
+var minDec = document.getElementById('minDec');
+var minUni = document.getElementById('minUni');
+var secDec = document.getElementById('secDec');
+var secUni = document.getElementById('secUni');
+var milDec = document.getElementById('milDec');
+var milUni = document.getElementById('milUni');
 
 
 function printTime() {
 
+    printMinutes();
+    printSeconds();
+
 }
 
 function printMinutes() {
-
+    const minutesPrint = chronometer.minutes
+    minDec.innerHTML = minutesPrint[0];
+    minUni.innerHTML = minutesPrint[1];
 }
 
 function printSeconds() {
-
+    const secondPrint = chronometer.seconds
+    console.log(secondPrint);
+    secDec.innerHTML = secondPrint[0];
+    secUni.innerHTML = secondPrint[1]
 }
 
 function printMilliseconds() {
@@ -51,7 +59,23 @@ function setResetBtn() {
 
 // Start/Stop Button
 btnLeft.addEventListener('click', function () {
-
+    if (btnLeft.classList.contains("start")) { // crono not running
+        btnLeft.innerHTML = "STOP";
+        btnLeft.classList.remove("start");
+        btnLeft.classList.add("stop")
+        btnRight.innerHTML = "SPLIT";
+        btnRight.classList.add("split");
+        btnRight.classList.remove('reset');
+        chronometer.startClick();
+    } else if (this.classList.contains("stop")) { //chrono is running
+        btnLeft.innerHTML = "START";
+        btnLeft.classList.remove("stop");
+        btnLeft.classList.add("start");
+        btnRight.innerHTML = "STOP"
+        btnRight.classList.add('reset');
+        btnRight.classList.remove('split');
+        chronometer.stopClick();
+    }
 });
 
 // Reset/Split Button
